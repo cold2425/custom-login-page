0da58e9d4d842c7db4470decdff626ee390837e1
cold2425 <pawel.warachowski@fibbo.pl>
2022-03-31 16:30:38 +0200

    Initial commit

---
+++ custom-login-page.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: Custom Login Page
+ * Plugin URI: http://www.mywebsite.com/my-first-plugin
+ * Description: The very first plugin that I have ever created.
+ * Version: 1.0
+ * Author: Your Name
+ * Author URI: http://www.mywebsite.com
+ */
